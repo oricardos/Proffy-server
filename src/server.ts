68a041270029065724b45ef77d340a7a2c2472cc
Luiz Ricardo Silva <ricardo.rs95@gmail.com>
2020-08-07 00:09:56 -0300

    database creation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,23 @@
-import express from 'express';
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.listen();
+app.use(express.json())
+
+// MÉTODOS HTTP
+// GET: Buscar ou listar uma informação
+// POST: Criar alguma nova informação
+// PUT: Atualizar uma informação existente
+// DELETE: Deletar uma informação existente
+
+// PARÂMETROS
+// Corpo (Request Body): Dados para criação ou atualização de um registro
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar
+// Query Params: Paginação, filtros, ordenação
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'hello'})
+})
+
+app.listen(3333)
 
